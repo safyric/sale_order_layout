d3c3f30b4423b25afd1a2e483ee6a66fba1f57ad
jchanme <39323414+jchanme@users.noreply.github.com>
2018-05-28 22:28:05 +0800

    Update sale_order.py

--- models/sale_order.py
+++ models/sale_order.py
@@ -14,3 +14,10 @@ class SaleOrder(models.Model):
     lead_time = fields.Float(
         'Lead Time', required=True, default=0.0,
         help="Number of days between the order confirmation and the shipping of the products to the customer")
+
+    use_payment_note = fields.Boolean(string='Payment Term Note')
+    payment_term_note = fields.Text(string='Payment Term Note')
+    use_incoterms_note = fields.Boolean(string='Incoterms Note')
+    incoterms_note = fields.Text(string='Incoterms Note')
+    use_leadtime_note = fields.Boolean(string='Lead Time Note')
+    leadtime_note = fields.Text(string='Lead Time Note')
